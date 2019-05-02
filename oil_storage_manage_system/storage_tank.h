f82c4afe8e5ba7fb9e590e074fa3103475cf0fbf
v.dunaev <dunvladyur4@gmail.com>
2019-05-02 15:05:40 +0300

    Designed structure and functions for working with tanks

---
+++ oil_storage_manage_system/storage_tank.h
@@ -0,0 +1,147 @@
+#ifndef OIL_STORAGE_MANAGE_SYSTEM_STORAGE_TANK_H
+#define OIL_STORAGE_MANAGE_SYSTEM_STORAGE_TANK_H
+
+#define STORAGE_TANK_ON 1
+#define STORAGE_TANK_OFF 0
+
+/**
+ * резервуар для хранения нефтепродуктов
+ */
+struct _storge_tank;
+typedef struct _storge_tank storage_tank;
+
+/**
+ * Создать резервуар
+ * @param min_level минимально допустиммый уровень нефти
+ * @param max_level максимально допустимый уровень нефти
+ * @return указатель на созданный резервуар
+ */
+storage_tank* create_storage_tank(unsigned int min_level, unsigned int max_level);
+
+/**
+ * переключить резервуар в рабочее состояние
+ * @param st указатель на резервуар
+ */
+void turn_on(storage_tank* st);
+
+/**
+ * переключить резервуар в нерабочее состояние
+ * @param st указатель на резервуар
+ */
+void turn_off(storage_tank* st);
+
+/**
+ * получить состояние работы резервуара
+ * @param st указатель на резервуар
+ * @return STORAGE_TANK_OFF - нерабочее состояние, STORAGE_TANK_ON - рабочее состояние
+ */
+int get_state(storage_tank* st);
+
+/**
+ * установить мимнимальный уровень нефти
+ * @param st указатель на резервуар
+ * @param min_level минимальный уровень нефти
+ */
+void set_minimum_level(storage_tank* st, unsigned int min_level);
+
+/**
+ * получить минимальный уровень нефти
+ * @param st указатель на резервуар
+ * @return минимальный уровень нефти
+ */
+unsigned int get_minimum_level(storage_tank* st);
+
+/**
+ * установить максимальный уровень нефти
+ * @param st указатель на резервуар
+ * @param min_level максимальный уровень нефти
+ */
+void set_maximum_level(storage_tank* st, unsigned int max_level);
+
+/**
+ * получить максимальный уровень нефти
+ * @param st указатель на резервуар
+ * @return максимальный уровень нефти
+ */
+unsigned int get_maximum_level(storage_tank* st);
+
+/**
+ * получить уровень нефти
+ * @param st указатель на резервуар
+ * @return уровень нефти
+ */
+unsigned int get_curent_level(storage_tank* st);
+
+/**
+ * уничтожить резевуар
+ * @param st указатель на резервуар
+ */
+void finalize_storage_tanK(storage_tank* st);
+
+/**
+ * включить насос закачки нефтепродуктов
+ * @param st указатель на резервуар
+ */
+void turn_on_injection_pump(storage_tank* st);
+
+/**
+ * выключить насос закачки нефтепродуктов
+ * @param st указатель на резервуар
+ */
+void turn_off_injection_pump(storage_tank* st);
+
+/**
+ * получить состояние работы насоса закачки
+ * @param st указатель на резервуар
+ * @return 0 - нерабочее состояние, 1 - рабочее состояние
+ */
+int get_state_injection_pump(storage_tank* st);
+
+/**
+ * установить скорость закачки нефтепродуктов
+ * @param st указатель на резервуар
+ * @param speed скорость закачки
+ */
+void set_speed_injection_pump(storage_tank* st, unsigned int speed);
+
+/**
+ * получить скорость закачки нефтепродуктов
+ * @param st указатель на резервуар
+ * @return скорость закачки нефтепродуктов
+ */
+unsigned int get_speed_injection_pump(storage_tank* st);
+
+/**
+ * включить насос откачки нефтепродуктов
+ * @param st указатель на резервуар
+ */
+void turn_on_pumping_pump(storage_tank* st);
+
+/**
+ * выключить насос откачки нефтепродуктов
+ * @param st указатель на резервуар
+ */
+void turn_off_pumping_pump(storage_tank* st);
+
+/**
+ * получить состояние работы насоса откачки
+ * @param st указатель на резервуар
+ * @return 0 - нерабочее состояние, 1 - рабочее состояние
+ */
+int get_state_pumping_pump(storage_tank* st);
+
+/**
+ * установить скорость откачки нефтепродуктов
+ * @param st указатель на резервуар
+ * @param speed скорость откачки
+ */
+void set_speed_pumping_pump(storage_tank* st, unsigned int speed);
+
+/**
+ * получить скорость откачки нефтепродуктов
+ * @param st указатель на резервуар
+ * @return скорость откачки нефтепродуктов
+ */
+unsigned int get_speed_pumping_pump(storage_tank* st);
+
+#endif //OIL_STORAGE_MANAGE_SYSTEM_STORAGE_TANK_H
